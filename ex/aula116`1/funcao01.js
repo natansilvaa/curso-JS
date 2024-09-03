fb74f9f96509a6e44b53f3c4a6da94e4376455e5
natansilvaa <natan.picanco24@gmail.com>
2024-09-03 19:58:05 -0300

    ex sobre funções

---
+++ ex/aula116`1/funcao01.js
@@ -0,0 +1,12 @@
+function parinp(){
+    if(n%2 ==0) {
+        retur `par!`
+    } else {
+        return `impar!`
+    }
+}
+
+// let res = parinpar(4)
+// console.log(res)
+
+console.log(parinp())
---
+++ ex/aula116`1/funcao02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0){
+return n1+n1
+}
+
+console.log(soma())
